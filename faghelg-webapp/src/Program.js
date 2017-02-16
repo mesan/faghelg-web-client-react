eff1fddc62182638207c46acc7cf606e474d6f5b
Jan-Ole Bårdevik <janb@mesan.no>
2017-02-16 18:53:14 +0100

    Lagt til routing på webappen

---
+++ faghelg-webapp/src/Program.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './program.css';
+
+class Program extends Component {
+    render() {
+        return (
+            <div className="program">
+                <h1>Program kommer her</h1>
+            </div>
+        );
+    }
+}
+
+export default Program;
--- faghelg-webapp/src/index.js
+++ faghelg-webapp/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Program from './Program'
 import './index.css';
+// using an ES6 transpiler, like babel
+import { Router, Route, Link, browserHistory } from 'react-router'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="/program" component={Program}/>
+        </Route>
+    </Router>
+), document.getElementById('root'))
