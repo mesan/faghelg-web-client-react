e6696d99b2c216d55f3a3a4c361951f12ec47a8d
Jan-Ole Bårdevik <janb@mesan.no>
2017-03-20 16:19:17 +0100

    Pyntet på uthenting av Program, bruker nå moments for konvertering av unix time

--- faghelg-webapp/src/program/Program.js
+++ faghelg-webapp/src/program/Program.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import './program.css';
+import React, {Component} from "react";
+import "./program.css";
+import moment from "moment";
 
 const StartTime = props =>
-    <div className="startTime">{props.start}</div>
+    <div className="startTime">{props.start}</div>;
 
 const EndTime = props =>
-    <div className="endTime">{props.end}</div>
+    <div className="endTime">{props.end}</div>;
 
 const Title = props =>
-    <div className="title">{props.title}</div>
+    <div className="title">{props.title}</div>;
 
 const HostNames = props =>
-    <div className="hostNames">{props.hostNames}</div>
+    <div className="hostNames">{props.hostNames}</div>;
 
 const EventImageUrl = props =>
-    <div><img className="eventImageUrl" src={props.eventImageUrl} /></div>
+    <div><img className="eventImageUrl" src={props.eventImageUrl} /></div>;
 
 const Event = ({
     start,
@@ -24,19 +25,33 @@ const Event = ({
     eventImageUrl
 }) =>
     <div className="eventContainer">
-        <EventImageUrl eventImageUrl={eventImageUrl}></EventImageUrl>
-        <Title title={title} />
-        <StartTime start={start}/>
-        <EndTime end={end}/>
-        <HostNames hostNames={hostNames} />
-    </div>
+        <div className="eventImageContainer">
+            <EventImageUrl eventImageUrl={eventImageUrl}/>
+        </div>
+        <div className="eventContentContainer">
+            <div className="eventTitleContainer">
+                <Title title={title} />
+            </div>
+            <div className="eventTimeContainer">
+                <div className="startTimeContainer">
+                    <StartTime start={moment.unix(start).format('LLL')}/>
+                </div>
+                <div className="endTimeContainer">
+                    <EndTime end={moment.unix(end).format('LLL')}/>
+                </div>
+            </div>
+            <div className="eventHostnameContainer">
+                <HostNames hostNames={hostNames} />
+            </div>
+        </div>
+    </div>;
+
+
 
 const Events = ({events}) =>
     <div>
         {events.map((event, index) => <Event key={index} {...event} />)}
     </div>;
-
-
 
 class Program extends Component {
     constructor(props) {
@@ -44,22 +59,27 @@ class Program extends Component {
         this.state = {
             events : []
         };
+
     }
 
     componentDidMount() {
-        fetch('http://faghelg.herokuapp.com/program')
+        console.log(fetch('http://faghelg.herokuapp.com/program')
             .then(response => response.json())
-            .then(response =>
-                // console.log(response.events)
-                this.setState({events:response.events})
-            )
-            .catch(err => console.error('Failed to get program at this time.', err))
+            //.then(response => console.log(response.events))
+            .then(response => this.setState({events:response.events}))
+            .catch(err => console.error('Failed to get program at this time.', err)))
     }
 
     render() {
+        moment.locale("nb");
+
         return (
             <div className="program">
                 <h1>Program</h1>
+
+                <button>3. november</button>
+                <button>4. november</button>
+                <button>5. november</button>
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8">
@@ -70,8 +90,6 @@ class Program extends Component {
             </div>
         );
     }
-
-
 }
 
 export default Program;
