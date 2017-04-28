a9c0f3b78fba2d1955f2e39655f9333e09894079
Jan-Ole Bårdevik <janb@mesan.no>
2017-04-28 09:12:57 +0200

    Lagt til søk på personer

--- faghelg-webapp/src/persons/Persons.js
+++ faghelg-webapp/src/persons/Persons.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import './persons.css';
+import React, {Component} from "react";
+import "./persons.css";
 
 const PersonFullName = props =>
-  <div className="personName">{props.children}</div>
+  <div className="personName">{props.children}</div>;
 
 const ImageUrl = props =>
-  <img className="profileImg" src={props.children} />
+  <img className="profileImg" src={props.children} />;
 
 const Person = ({
   fullName,
@@ -15,7 +15,7 @@ const Person = ({
   <div className="personContainer">
     <ImageUrl>{profileImageUrl}</ImageUrl>
     <PersonFullName>{fullName}</PersonFullName>
-  </div>
+  </div>;
 
 const Persons = ({persons}) =>
   <div>
@@ -33,27 +33,48 @@ class PersonList extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        persons : []
+        persons : [],
+        filterString: ""
       };
+      this.filterPersons = this.filterPersons.bind(this);
+      this.onChange = this.onChange.bind(this);
     }
 
     componentDidMount() {
       fetch('http://faghelg.herokuapp.com/persons')
       .then(response => response.json())
       .then(persons =>
-        this.setState({persons})
+        this.setState({persons: persons})
       )
       .catch(err => console.error('Failed to fetch persons.', err));
+  }
+
+  filterPersons(person) {
+
+      if (this.state.filterString === "") {
+          return true;
+      }
+      if (person.fullName.includes(this.state.filterString)) {
+          return true;
+      }
+      return false;
+  }
+
+  onChange(event) {
+    this.setState({filterString: event.target.value});
   }
 
     render() {
         return (
             <div className="persons">
                 <h1 className="pageHeader">Personer</h1>
+                <div className="searchContainer" >
+                    <input type="text" value={this.state.filterString} placeholder="Søk" onChange={this.onChange}/>
+                </div>
                 <div className="container">
                   <div className="row">
                     <div>
-                        <Persons persons={this.state.persons} />
+                        <Persons persons={this.state.persons.filter(this.filterPersons)} />
                     </div>
                   </div>
               </div>
